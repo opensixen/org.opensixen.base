3531791acff53b3eda0cf7aaa59f1e3de7dae2e7
Eloy Gomez <eloy@indeos.es>
2011-05-13 11:26:20 +0000

    Revertimos cambio anterior, no permite generar albaranes si no coincide con el conjunto de atributos especificado.

--- src/org/compiere/model/MOrderLine.java
+++ src/org/compiere/model/MOrderLine.java
@@ -927,7 +927,7 @@ public class MOrderLine extends X_C_OrderLine
 		
 		//	R/O Check - Product/Warehouse Change
 		if (!newRecord 
-			&& (is_ValueChanged("M_Product_ID") || is_ValueChanged("M_Warehouse_ID") || is_ValueChanged(COLUMNNAME_M_AttributeSetInstance_ID))) 
+			&& (is_ValueChanged("M_Product_ID") || is_ValueChanged("M_Warehouse_ID"))) 
 		{
 			if (!canChangeWarehouse())
 				return false;
